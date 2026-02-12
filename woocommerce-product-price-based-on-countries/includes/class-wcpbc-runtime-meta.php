d62e0cc855ee1ef71c783802fef72bd899126870
Sean Murphy <sean@iamseanmurphy.com>
2026-02-12 10:28:14 +0000

    4.2.1

---
+++ woocommerce-product-price-based-on-countries/includes/class-wcpbc-runtime-meta.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Dynamic properties for WC_Data objects using meta.
+ *
+ * Allows to store additional properties on WC_Data objects that expires on the object destroy.
+ *
+ * @since 4.2.0
+ * @package WC_BOGOF
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WCPBC_Runtime_Meta class
+ */
+class WCPBC_Runtime_Meta {
+
+	/**
+	 * Init hooks.
+	 */
+	public static function init() {
+		add_filter( 'add_post_metadata', [ __CLASS__, 'reject_metadata' ], 9999, 3 );
+	}
+
+	/**
+	 * Do not update the runtime meta.
+	 *
+	 * @param null|bool $check      Whether to allow adding metadata for the given type.
+	 * @param int       $post_id    ID of the object metadata is for.
+	 * @param string    $meta_key   Metadata key.
+	 */
+	public static function reject_metadata( $check, $post_id, $meta_key ) {
+		if ( __CLASS__ === $meta_key ) {
+			$check = true;
+		}
+		return $check;
+	}
+
+	/**
+	 * Reads the meta data.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @return array
+	 */
+	private static function read( $data ) {
+		$meta = $data->get_meta( __CLASS__, true, 'edit' );
+		$meta = is_array( $meta ) ? $meta : [];
+
+		return $meta;
+	}
+
+	/**
+	 * Updates the meta data.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @param array   $meta Data to save.
+	 */
+	private static function update( $data, $meta ) {
+		$data->update_meta_data(
+			__CLASS__,
+			$meta
+		);
+	}
+
+	/**
+	 * Returns a property by key.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @param string  $key Property name.
+	 * @return mixed
+	 */
+	public static function get( $data, $key ) {
+		$meta = self::read( $data );
+		return isset( $meta[ $key ] ) ? $meta[ $key ] : false;
+	}
+
+	/**
+	 * Sets a property by key.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @param string  $key Property name.
+	 * @param mixed   $value Value.
+	 */
+	public static function set( $data, $key, $value ) {
+		$meta         = self::read( $data );
+		$meta[ $key ] = $value;
+		self::update( $data, $meta );
+	}
+
+	/**
+	 * Unsets a property by key.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @param string  $key Property name.
+	 */
+	public static function unset( $data, $key ) {
+		$meta = self::read( $data );
+		unset( $meta[ $key ] );
+		self::update( $data, $meta );
+	}
+
+	/**
+	 * Check if a propery is set.
+	 *
+	 * @param WC_Data $data WC_Data object.
+	 * @param string  $key Property name.
+	 */
+	public static function isset( $data, $key ) {
+		$meta = self::read( $data );
+		return isset( $meta[ $key ] );
+	}
+}
--- woocommerce-product-price-based-on-countries/readme.txt
+++ woocommerce-product-price-based-on-countries/readme.txt
@@ -4,7 +4,7 @@ Tags:  woocommerce, country-based pricing, price by country, geolocation, multi 
 Requires at least: 3.8
 Tested up to: 6.9
 Requires PHP: 7.0
-Stable tag: 4.2.0
+Stable tag: 4.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -130,6 +130,9 @@ Please review [How to test the pricing zones](https://www.pricebasedcountry.com/
 7. Includes a country selector widget.
 
 == Changelog ==
+
+= 4.2.1 (2026-02-12) =
+* Fixed: WCPBC_Runtime_Meta" not found.
 
 = 4.2.0 (2026-02-12) =
 * Added: Compatible with WooCommerce 10.5+.
